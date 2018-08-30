422d9c5eef45a2c11ed1202ab1aaf9c6765f5315
adonis <adonis@arisant.com>
2018-08-30 16:56:56 +0300

    First commit

---
+++ src/main/java/dozer/test/NestedMessage.java
@@ -0,0 +1,45 @@
+package dozer.test;
+
+public class NestedMessage
+{
+	public static class Inner {
+
+		private String prop1;
+		
+		public Inner() {
+		}
+		
+		public String getProp1() {
+			return prop1;
+		}
+		
+		public void setProp1(String prop1) {
+			this.prop1 = prop1;
+		}
+	}
+	
+	public static class Outer {
+		private String prop2;
+		private Inner prop1;
+		
+		public Outer(){
+			
+		}
+		
+		public String getProp2() {
+			return prop2;
+		}
+		
+		public void setProp2(String prop2) {
+			this.prop2 = prop2;
+		}
+		
+		public Inner getProp1() {
+			return prop1;
+		}
+		
+		public void setProp1(Inner prop1) {
+			this.prop1 = prop1;
+		}
+	}
+}
---
+++ src/test/java/dozer/test/NestedMessagesTest.java
@@ -0,0 +1,72 @@
+package dozer.test;
+
+import org.junit.Test;
+
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
+import com.github.dozermapper.core.loader.api.BeanMappingBuilder;
+
+import dozer.test.NestedMessage.Inner;
+import dozer.test.NestedMessage.Outer;
+
+public class NestedMessagesTest
+{
+	
+	
+	@Test
+	public void testNestedMessage()
+	{
+		Mapper mapper = DozerBeanMapperBuilder.create().withMappingBuilders(
+				
+				new BeanMappingBuilder() {
+
+					@Override
+					protected void configure() {
+						
+						mapping(Inner.class, test.api.v1.Outer.Inner.class);
+						mapping(Outer.class, test.api.v1.Outer.class);
+						
+					}
+					
+				}
+				
+		).build();
+		
+		Outer o = new Outer();
+		Inner i = new Inner();
+		i.setProp1("1.1");
+		o.setProp1(i);
+		o.setProp2("1");
+		
+		mapper.map(o, test.api.v1.Outer.class);
+	}
+	
+	@Test
+	public void testFlatMessage()
+	{
+		Mapper mapper = DozerBeanMapperBuilder.create().withMappingBuilders(
+				
+				new BeanMappingBuilder() {
+
+					@Override
+					protected void configure() {
+						
+						mapping(Inner.class, test.api.v1.FlatInner.class);
+						mapping(Outer.class, test.api.v1.FlatOuter.class);
+						
+					}
+					
+				}
+				
+		).build();
+		
+		Outer o = new Outer();
+		Inner i = new Inner();
+		i.setProp1("1.1");
+		o.setProp1(i);
+		o.setProp2("1");
+		
+		mapper.map(o, test.api.v1.FlatOuter.class);
+		
+	}
+}
